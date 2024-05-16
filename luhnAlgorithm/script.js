8ed3992f64ede26fe4751c1b10760e10010b4ad8
Davut Dağtekin <142156294+Dvdt5@users.noreply.github.com>
2024-05-16 19:40:38 +0300

    Add files via upload

--- luhnAlgorithm/script.js
+++ luhnAlgorithm/script.js
@@ -1,15 +1,40 @@
-const userToDoNote = document.getElementById("toDo");
-const userToDoTime = document.getElementById("time");
-const submitButton = document.getElementById("submit");
-const notesList = document.getElementById("showList");
+const submitBtn = document.getElementById("submit-btn");
+const userInput = document.getElementById("cc-number");
+const textMsg = document.getElementById("answer");
 
 
+submitBtn.addEventListener("click",() => {
+    //Create regex for checking numbers
+    const regex = /[^0-9]/g;
+    if (userInput.value.match(regex) !== null) {
+        textMsg.innerText = "Please enter only numbers!";
+        return;
+    }
 
-function addNote() {
+    //If statement for checking numbers length
+    if ((userInput.value.split("")).length !== 16) {
+        textMsg.innerText = "Please enter a 16 digit number.";
+        return;
+    }
+
+    //Making the input an array so we can iterate numbers and reverse it so we start counting from right
+    let userArr = userInput.value.split("").reverse();
+    let sum = 0;
     
-    let HTMLString = `<p class="toDoRow">${userToDoNote.value}</p><span class="right"><button class="mark button">Mark as done</button><button class="delete button">Delete</button></span>
-    <hr>`;
-    notesList.insertAdjacentHTML("beforeend",HTMLString);
-    
-}
-submitButton.addEventListener("click",addNote);
+    for (let i = 0; i < 16;i++){
+        let makeDataNumber = Number(userArr[i])
+        if (i % 2 === 0) {
+            sum += makeDataNumber;
+        } else if ((makeDataNumber*2) > 9) {
+            sum += (makeDataNumber * 2) - (9);
+        } else {
+            sum += makeDataNumber * 2;
+        }
+    }
+    if (sum % 10 !== 0) {
+    textMsg.innerText = `${userInput.value} is not a valid Credit Card number.`;
+    } else {
+    textMsg.innerText = `${userInput.value} is a valid Credit Card number.`;
+    }
+
+});
